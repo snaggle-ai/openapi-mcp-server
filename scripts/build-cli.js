180adfe8ea79436804705af55e352874f5b2ebdb
Snaggle.ai <info@snaggle.ai>
2025-01-11 23:02:19 +0100

    v1.1.0

--- scripts/build-cli.js
+++ scripts/build-cli.js
@@ -11,16 +11,17 @@ async function build() {
     bundle: true,
     minify: true,
     platform: 'node',
-    target: 'node16',
+    target: 'node18',
     format: 'esm',
     outfile: './dist/cli.mjs',
     banner: {
-      js: '#!/usr/bin/env node',
+      js: "#!/usr/bin/env node\nimport { createRequire } from 'module';const require = createRequire(import.meta.url);" // see https://github.com/evanw/esbuild/pull/2067
     },
+    external: ['util'],
   });
 
   // Make the output file executable
-  await chmod('./dist/cli.cjs', 0o755);
+  await chmod('./dist/cli.mjs', 0o755);
 }
 
 build().catch((err) => {
